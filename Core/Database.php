38230500a942a2b8eb29f84f7d86417ef1a7022a
Dorian Wysocki <freakdorian@gmail.com>
2020-12-12 16:43:26 +0100

    Change constructor visibility

--- Core/Database.php
+++ Core/Database.php
@@ -5,7 +5,7 @@ class Database
     private static $instance = null;
     private $db;
 
-    private function __construct()
+    public function __construct()
     {
         $host = DB_HOST;
         $user = DB_USER;
